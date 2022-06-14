5ad5ba0b3e30250c54212401388de0ae23c83650
muxammadziz <muxammadazizramziddinov@gmail.com>
2022-06-14 13:15:32 +0500

    commit

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const validation = require('../middleware/validation.js')
-const { Register, Login } = require('../controllers/authorization')
+const { Register, Login, Get } = require('../controllers/users')
 
 let router = express.Router()
 
 router.post('/register', Register)
 router.post('/login', Login)
+router.get('/users', Get)
 
 
 module.exports = router
--- src/routes/video.js
+++ src/routes/video.js
@@ -1,11 +1,15 @@
 const express = require('express')
-const { getVideo, postVideo } = require('../controllers/video')
+const { getVideo, postVideo, view, download } = require('../controllers/video')
 const checkToken = require('../middleware/checkToken')
 
 let router = express.Router()
 
 
-router.get('/videos', getVideo)
 router.post('/videoUpload', checkToken, postVideo)
+router.get('/videos', getVideo)
+router.get('/view/:fileName', view)
+router.get('/download/:fileName', download)
+
+
 
 module.exports =  router
